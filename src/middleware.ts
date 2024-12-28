158ebde604b4cf6b3b888d276e724a05de596703
Uros Petrovic <132246925+urosptrvc@users.noreply.github.com>
2024-12-28 02:08:46 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,34 @@
 // /middleware.ts
-import { NextRequest, NextResponse } from "next/server"
-import { getToken } from "next-auth/jwt"
+import { NextRequest, NextResponse } from "next/server";
+import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-    const protectedRoutes = ["/shipments", "/shipments/upload"]
+    const protectedRoutes = [
+        "/shipments",
+        "/shipments/upload",
+        "/api/shipments",
+        "/api/shipments/upload"
+    ];
 
-    if (protectedRoutes.some(route => req.nextUrl.pathname.startsWith(route))) {
+    if (
+        protectedRoutes.some(route => req.nextUrl.pathname.startsWith(route)) ||
+        req.nextUrl.pathname.match(/^\/shipments\/\w+/) || // Dinamičke rute shipments/[id]
+        req.nextUrl.pathname.match(/^\/api\/shipments\/\w+/) // Dinamičke API rute api/shipments/[id]
+    ) {
         const session = await getToken({
             req,
             secret: process.env.NEXTAUTH_SECRET
-        })
+        });
         if (!session) {
-            return NextResponse.redirect(new URL("/auth/login", req.url))
+            return NextResponse.redirect(new URL("/auth/login", req.url));
         }
     }
-    return NextResponse.next()
+    return NextResponse.next();
 }
 
 export const config = {
-    matcher: ["/shipments/:path*"]
-}
+    matcher: [
+        "/shipments/:path*",
+        "/api/shipments/:path*"
+    ]
+};
